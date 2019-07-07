bb27c4ef2ddd3cb2f65859c572ecd68fda3a1332
rachel ng <rachel.ntc@gmail.com>
2019-07-08 01:02:14 +0800

    AppTest project created

---
+++ AppTest/AppTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppTest
+//
+//  Created by rachel ng on 08/07/2019.
+//  Copyright © 2019 rachelng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
